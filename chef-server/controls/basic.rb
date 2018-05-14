8786a100871a3a920952ff256505ff9c1a11914c
Will Fisher <wfisher@chef.io>
2018-05-14 16:15:00 -0700

    revamp the disk checks

--- chef-server/controls/basic.rb
+++ chef-server/controls/basic.rb
@@ -15,42 +15,20 @@ control "chef-server.gatherlogs.chef-server" do
 end
 
 df = disk_usage()
-required_available_on_var = 10
-required_size_on_var = 40
 
-control "chef-server.gatherlogs.disk_usage" do
-  title "check that the chef-server has plenty of free space"
-  desc "
-    there are several key directories that we need to make sure have enough
-    free space for chef-server to operate succesfully
-  "
+%w(/ /var /var/opt /var/opt/opscode /var/log).each do |mount|
+  control "chef-server.gatherlogs.critical_disk_usage.#{mount}" do
+    title "check that the chef-server has plenty of free space"
+    desc "
+      there are several key directories that we need to make sure have enough
+      free space for chef-server to operate succesfully
+    "
 
-  describe df.mount('/') do
-    its('used_percent') { should cmp < 100 }
-    if df.mount('/var')
-      # make sure that is a least 1 gig free on / if we have /var
+    only_if { df.exists?(mount) }
+
+    describe df.mount(mount) do
+      its('used_percent') { should cmp < 100 }
       its('available') { should cmp > 1 }
-    else
-      # no /var so use same rules as /var
-      its('available') { should cmp > required_available_on_var }
-      its('size') { should cmp > required_size_on_var }
-    end
-  end
-
-  if df.mount('/var')
-    describe df.mount('/var') do
-      its('used_percent') { should cmp < 100 }
-      its('available') { should cmp > required_available_on_var }
-      its('size') { should cmp > required_size_on_var }
-    end
-  end
-
-  # sometimes users have a separate mount for /var/opt/opscode, lets check that
-  if df.mount('/var/opt/opscode')
-    describe df.mount('/var/opt/opscode') do
-      its('used_percent') { should cmp < 100 }
-      its('available') { should cmp > required_available_on_var }
-      its('size') { should cmp > required_size_on_var }
     end
   end
 end
