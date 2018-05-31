09df0c3141686911ced5324b6be1b5a38c6f49e7
Will Fisher <wfisher@chef.io>
2018-05-31 09:39:36 -0700

    adds check for hostname error for push-jobs server

---
+++ chef-server/controls/push_jobs_issues.rb
@@ -0,0 +1,28 @@
+pushjobs = installed_packages('opscode-push-jobs-server')
+
+
+
+%w{ access.log current error.log }.each do |logfile|
+  pushjobs_hostname = log_analysis("var/log/opscode/nginx/#{logfile}", 'host not found in upstream .* in /var/opt/opscode/nginx/etc/addon.d/10-push_jobs_upstreams.conf')
+
+  control "chef-server.gatherlogs.push-jobs-server-hostname-misconfigured-#{logfile}" do
+    title "Check for misconfiruation for hostname of push-jobs server"
+    desc "
+    Nginx is unable to communicate with the push-jobs service.
+
+    Found: #{pushjobs_hostname.first}
+
+    This can happen if the hostname of the chef-server is changed but
+    `opscode-push-jobs-server-ctl reconfigure` was not run to update the nginx
+    configs.
+    "
+
+    impact 1.0
+
+    only_if { pushjobs.exists? }
+
+    describe pushjobs_hostname do
+      it { should_not exist }
+    end
+  end
+end
