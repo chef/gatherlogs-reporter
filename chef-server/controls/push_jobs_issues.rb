732bfec78ca620a7842081c33458e79a96032c05
Will Fisher <will@teknofire.net>
2018-06-05 14:13:17 -0700

    adding check for push job clients with bad timestamp errors (#6)

--- chef-server/controls/push_jobs_issues.rb
+++ chef-server/controls/push_jobs_issues.rb
@@ -1,6 +1,6 @@
 pushjobs = installed_packages('opscode-push-jobs-server')
 
-%w{ access.log current error.log }.each do |logfile|
+%w{ access.log current error.log jobs.log }.each do |logfile|
   pushjobs_hostname = log_analysis("var/log/opscode/nginx/#{logfile}", 'host not found in upstream .* in /var/opt/opscode/nginx/etc/addon.d/10-push_jobs_upstreams.conf')
 
   control "gatherlogs.chef-server.push-jobs-server-hostname-misconfigured-#{logfile}" do
@@ -17,9 +17,28 @@ pushjobs = installed_packages('opscode-push-jobs-server')
 
     impact 1.0
 
-    only_if { pushjobs.exists? }
+    only_if { pushjobs.exists? && pushjobs_hostname.log_exists? }
 
     describe pushjobs_hostname do
+      it { should_not exist }
+    end
+  end
+
+  pushjobs_timestamp = log_analysis("var/log/opscode/opscode-pushy-server/#{logfile}", 'Bad timestamp in message')
+
+  control "gatherlogs.chef-server.push-jobs-server-bad-timestamp-#{logfile}" do
+    title "Check for messages about bad timestamps for push jobs clients"
+    desc "
+      Found '#{pushjobs_timestamp.grep_expr}', #{pushjobs_timestamp.hits} times in #{logfile}
+
+      This usually indicates that a push-jobs client has an incorrect date/time set.
+
+      See: https://getchef.zendesk.com/hc/en-us/articles/208496996-Push-Jobs-Jobs-Crashing-Consistently
+    "
+
+    only_if { pushjobs.exists? && pushjobs_timestamp.log_exists? }
+
+    describe pushjobs_timestamp do
       it { should_not exist }
     end
   end
