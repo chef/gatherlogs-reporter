d590b2d6ca8353167be0768151b56e049c1e6095
Will Fisher <wfisher@chef.io>
2018-07-11 13:30:50 -0700

    fix checking for any error and not <= 10
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- automate2/controls/issues.rb
+++ automate2/controls/issues.rb
@@ -3,15 +3,15 @@
 upgrade_failed = log_analysis("journalctl_chef-automate.txt", 'level=error msg="Phase failed" error="hab-sup upgrade pending" phase="supervisor upgrade"', a2service: 'service.default')
 control "gatherlogs.automate2.upgrade_failed" do
   impact 1.0
-  title 'Automate is reporting a failure during the upgrade process'
+  title 'Check to see if Automate is reporting a failure during the hab sup upgrade process'
   desc "
 It appears that there was a failure during the upgrade process for Automate, please
 check the logs and contact support to see about getting this fixed.
-  
+
 #{upgrade_failed.summary}
   "
 
   describe upgrade_failed do
-    its('hits') { should cmp <= 10 }
+    its('last_entry') { should be_empty }
   end
 end
