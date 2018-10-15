ed1d0d9ef516a6cbbb72144d816c6d4698f3a858
Will Fisher <wfisher@chef.io>
2018-10-15 16:13:16 -0700

    better description for drbd detection

--- profiles/chef-server/controls/drbd.rb
+++ profiles/chef-server/controls/drbd.rb
@@ -2,10 +2,9 @@ control 'gatherlogs.chef-server.check_for_drbd' do
   impact 0.5
   title 'Check to see if the system is using legacy DRDB HA configuration'
   desc '
-It appears that the chef-server has a legacy DRBD configuration.
-This feature will be end-of-life for support on March 31, 2019.
+Chef-server is using a legacy DRBD configuration.
+This feature will reach end-of-life for support on March 31, 2019.
 '
-
   tag kb: 'https://blog.chef.io/2018/10/02/end-of-life-announcement-for-drbd-based-ha-support-in-chef-server/'
 
   describe file('private-chef-ctl_ha-status.txt') do
