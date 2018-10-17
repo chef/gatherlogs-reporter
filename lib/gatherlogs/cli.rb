f0e075e6c1bb119951500c261750321b925ea2ab
Will Fisher <will@teknofire.net>
2018-10-17 14:38:59 -0700

    Add habitat build plan (#39)
    * this now will build a habitat package
    * Use the run/build time methods for hab to set env
    * Set git tags for versions
    * fix loading of version file
    * dirty hack to allow docker export
    * allow downloading of remote files

--- lib/gatherlogs/cli.rb
+++ lib/gatherlogs/cli.rb
@@ -1,14 +1,20 @@
 require 'json'
 require 'mixlib/shellout'
 require 'clamp'
+require 'fileutils'
+
 require "gatherlogs/product"
+require "gatherlogs/reporter"
 
 PROFILES_PATH = File.realpath(File.join(File.dirname(__FILE__), '../../profiles')).freeze
 Clamp.allow_options_after_parameters = true
 
 module Gatherlogs
   class CLI < Clamp::Command
+    attr_accessor :current_log_path, :remote_cache_dir
+
     option ['-p', '--path'], 'PATH', 'Path to the gatherlogs for inspection', default: '.', attribute_name: :log_path
+    option ['-r', '--remote'], 'REMOTE_URL', 'URL to the remote tar bal for inspection', attribute_name: :remote_url
     option ['-d', '--debug'], :flag, 'Enable debug output'
     option ['--profiles'], :flag, 'Show available profiles'
     option ['-v', '--verbose'], :flag, 'Show inspec test output'
@@ -17,8 +23,17 @@ module Gatherlogs
 
     parameter "[PROFILE]", "profile to execute", attribute_name: :inspec_profile
 
+    def initialize(*args)
+      super
+
+      @current_log_path = nil
+      @remote_cache_dir = nil
+    end
+
     def execute()
       parse_args
+
+      current_log_path = log_path.dup
 
       if version?
         puts "#{File.basename($0)}: #{Gatherlogs::VERSION}"
@@ -35,12 +50,16 @@ module Gatherlogs
         exit 0
       end
 
+      current_log_path = fetch_remote_tar(remote_url) unless remote_url.nil?
+
+      debug_msg("Using log_path: #{current_log_path}")
+
       profile = nil
       if inspec_profile
         profile = inspec_profile.dup
       else
         status_msg "Attempting to detect gatherlogs product..."
-        profile = Gatherlogs::Product.detect(log_path)
+        profile = Gatherlogs::Product.detect(current_log_path)
         status_msg "Detected '#{profile}' files" unless profile.nil?
       end
 
@@ -55,10 +74,29 @@ module Gatherlogs
       })
 
       profile_path = find_profile_path(profile)
-      report_json = inspec_exec(profile_path)
+      report_json = inspec_exec(profile_path, current_log_path)
 
       status_msg "Generating report..."
       puts @reporter.report(report_json)
+    ensure
+      FileUtils.remove_entry remote_cache_dir
+    end
+
+    def fetch_remote_tar(url)
+      @remote_cache_dir = Dir.mktmpdir('gatherlogs')
+      
+      extension = remote_url.split('.').last
+      local_filename = File.join(remote_cache_dir, "gatherlogs.#{extension}")
+
+      cmd = ['wget', remote_url, '-O', local_filename]
+      shellout!(cmd)
+
+      debug_msg "Remote cache dir: #{remote_cache_dir}"
+
+      cmd = ['tar', 'xvfz', local_filename, '-C', remote_cache_dir, '--strip-components', '2']
+      shellout!(cmd)
+
+      remote_cache_dir
     end
 
     def find_profile_path(profile)
@@ -74,14 +112,14 @@ module Gatherlogs
       @log_level = :debug if debug?
     end
 
-    def inspec_exec(profile)
-      cmd = ['inspec', 'exec', profile, '--reporter', 'json'].join(' ')
+    def inspec_exec(profile, log_path)
+      cmd = ['inspec', 'exec', profile, '--no-create-lockfile', '--reporter', 'json']
 
       status_msg "Running inspec..."
       debug_msg('Executing', "'#{cmd}'")
 
       Dir.chdir(log_path) do
-        inspec = shellout(cmd, { returns: [0, 100, 101] })
+        inspec = shellout!(cmd, { returns: [0, 100, 101] })
         JSON.parse(inspec.stdout)
       end
     end
@@ -94,8 +132,8 @@ module Gatherlogs
       STDERR.puts(msg.join(' ')) if debug?
     end
 
-    def shellout(cmd, options={})
-      # puts cmd
+    def shellout!(cmd, options={})
+      puts cmd.join(' ') if debug?
       shell = Mixlib::ShellOut.new(cmd, options)
       shell.run_command
       shell.error!
