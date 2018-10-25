c8810d4fa310cea15e21c3a5650b7d341162a705
Will Fisher <wfisher@chef.io>
2018-10-25 14:50:23 -0700

    Add check for ES 1gb heap size
    Signed-off-by: Will Fisher <wfisher@chef.io>

---
+++ profiles/automate2/controls/elasticsearch.rb
@@ -0,0 +1,15 @@
+control 'gatherlogs.automate2.elasticsearch_heap_size' do
+  title 'Check that ElasticSearch is not configured with the default 1gig heap'
+
+  desc "
+ElasticSearch has been configured to use the default 1GB heap size and needs to
+be updated to be at least 50% of the available memory but no more than 32GB.
+"
+
+  tag kb: "https://automate.chef.io/docs/configuration/#setting-elasticsearch-heap"
+
+  describe file('hab/svc/automate-elasticsearch/config/jvm.options') do
+    its('content') { should_not match(/-Xms1g/) }
+    its('content') { should_not match(/-Xmx1g/) }
+  end
+end
