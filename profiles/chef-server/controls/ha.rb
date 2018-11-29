16315d8f3bffb648bdc474d151b8d1e0253d6e17
Will Fisher <will@teknofire.net>
2018-11-29 09:38:25 -0800

    Clean up sysinfo output (#73)
    Also adds a quick check for ldap configs
    
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- profiles/chef-server/controls/ha.rb
+++ profiles/chef-server/controls/ha.rb
@@ -3,10 +3,10 @@ topology = log_analysis('etc/opscode/chef-server.rb', 'topology')
 fe_node = log_analysis('etc/opscode/chef-server.rb', 'use_chef_backend true')
 
 system_info = { 'DRBD Enabled' => drbd.exist? ? 'Yes' : 'No' }
-system_info['Chef-HA Front-End'] = 'Yes' if fe_node.exists?
+system_info['HA Front-End'] = 'Yes' if fe_node.exists?
 system_info['Topology'] = topology.last.split(/\s+/).last if topology.exists?
 
-control '030.gatherlogs.chef-server.check_for_drbd' do
+control '041.gatherlogs.chef-server.check_for_drbd' do
   impact 0.5
   title 'Check to see if the system is using legacy DRDB HA configuration'
   desc '
