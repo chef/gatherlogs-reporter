d25483441bffa141a08067a6f47cc08ed1094d3d
Will Fisher <wfisher@chef.io>
2018-12-07 13:08:51 -0800

    Find when workflow runner terminated
    It's possible that the process that manages status of workflow's runners
    was terminated.  This finds when that happens
    
    Signed-off-by: Will Fisher <wfisher@chef.io>

---
+++ profiles/automate/controls/workflow.rb
@@ -0,0 +1,18 @@
+# you add controls here
+control 'gatherlogs.automate.runner_worker_terminated' do
+  impact 0.5
+  title 'Check to see the runner process was terminated'
+  desc '
+The Automate process that manage workers information was terminated.
+
+To resolve this issue restart the delivery service.
+  '
+
+  %w[console.log current].each do |logfile|
+    runner_worker_terminated = log_analysis(::File.join('var/log/delivery/delivery', logfile), 'terminated with reason:.*jobs_queue')
+    tag summary: runner_worker_terminated.summary
+    describe runner_worker_terminated do # The actual test
+      its('last_entry') { should be_empty }
+    end
+  end
+end
