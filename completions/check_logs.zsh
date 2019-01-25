e3886ba8f8115351c3979cc8a40741e003d81326
Will Fisher <wfisher@chef.io>
2019-01-25 11:12:39 -0800

    Renaming check_logs command to gatherlogs_report
    Creating symlinks so as not to break existing users
    
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- completions/check_logs.zsh
+++ completions/check_logs.zsh
@@ -1,10 +1,1 @@
-_gatherlog_profiles() {
-  local args
-  read -cA args
-
-  completions="$(check_logs --profiles)"
-
-  reply=("${(ps:\n:)completions}")
-}
-
-compctl -K _gatherlog_profiles -x 'c[-1,--path]' -f - 's[--]' -k '(path debug all profiles verbose version)' -- check_logs
+gatherlogs_report.zsh
