f8d72e5c5365b320370d92d8164624ba68e9bf60
Will Fisher <wfisher@chef.io>
2019-02-05 09:22:06 -0800

    Adding comment about what process is what av product
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- profiles/common/controls/antivirus.rb
+++ profiles/common/controls/antivirus.rb
@@ -4,6 +4,9 @@ control 'gatherlogs.common.antivirus_check' do
 The system is running an Antivirus agent, this is just an advisory message as
 occasionally these agents can interfere with or slow down internal processes."
 
+  # symantec - Symantec AV
+  # savd - Sophos AV
+  # ds_agent - TrendMicro
   %w[symantec ds_agent savd].each do |av_agent|
     agent_log = log_analysis('ps_fauxww.txt', av_agent)
     tag summary: agent_log.summary unless agent_log.empty?
