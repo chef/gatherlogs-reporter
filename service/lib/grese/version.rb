23a435f2c4c04a949b1c8e23b30cb6c15ee13913
Will Fisher <wfisher@chef.io>
2019-07-30 14:27:36 -0700

    Add gatherlogs service code
    Signed-off-by: Will Fisher <wfisher@chef.io>

---
+++ service/lib/grese/version.rb
@@ -0,0 +1,10 @@
+module Grese
+  BASE_PATH = File.join(File.dirname(__FILE__), '../..')
+  VERSION = File.read(File.join(BASE_PATH, 'VERSION'))
+
+  module Version
+    def self.version
+      "grese: #{Grese::VERSION}"
+    end
+  end
+end
