18ab5ad291a2001318f59095f66b4ad8714ab029
Will Fisher <wfisher@chef.io>
2021-08-05 10:47:53 -0700

    Update expeditor and buildkit setup
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- scripts/bk_container_prep.sh
+++ scripts/bk_container_prep.sh
@@ -1,7 +1,10 @@
 # Script to get a container ready to run the tests in BuildKite
-
+echo -n "Ruby version: "
+ruby --version
+echo -n "Gem version: "
 gem --version
-gem uninstall bundler -a -x || true
-gem install bundler -v 2.0.1
+# Ruby version is new enough that we don't need this now
+#gem uninstall bundler -a -x || true
+#gem install bundler -v 2.0.1
 bundle --version
 rm -f .bundle/config
